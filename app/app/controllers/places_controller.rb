1ff4fedb849ee03398e9768802e47a36024427c3
darshand <darshan.bd@onze.in>
2017-09-20 08:17:10 +0530

    Initial commit

---
+++ app/app/controllers/places_controller.rb
@@ -0,0 +1,29 @@
+class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+        render json: @places.to_json
+    end
+
+    def create
+        coords = Place.geo_factory.point(params.require(:place)[:lat],params.require(:place)[:lng])
+        @place = Place.new(place_params)
+        @place.coords = coords
+
+        if @place.save!
+            render json: {
+                status: 200,
+                message: "created"
+            }.to_json
+        else
+            render json: {
+                status: 500,
+                message: "error"
+            }.to_json
+        end
+    end
+
+    private
+    def place_params
+        params.require(:place).permit(:name)
+    end
+end
---
+++ app/app/models/place.rb
@@ -0,0 +1,5 @@
+class Place < ApplicationRecord
+def self.geo_factory
+    @geo_factory ||= RGeo::Geographic.spherical_factory(srid: 4326)
+end
+end
